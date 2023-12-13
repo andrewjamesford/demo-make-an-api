cd56990a5fd2161d73e3df42852e816379120e85
Andrew Ford <andrew@andrewford.co.nz>
2023-12-13 16:36:55 +1300

    Updated connection string for K8s

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ const app = require("./app");
 const port = process.env.PORT || 5001;
 const mongoose = require("mongoose");
 
-const dbURI = process.env.MONGO_URI || "mongodb://localhost:27017/mongo";
+let mongoUrlK8s = `mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@${process.env.DB_URL}`;
+// const mongoUrlK8s = process.env.MONGO_URI || "mongodb://localhost:27017/mongo";
+const dbURI = mongoUrlK8s;
 
 mongoose.connect(dbURI, {
     useNewUrlParser: true,
     serverSelectionTimeoutMS: 5000
-}).then(() => console.log("Connected to database")).catch((err) => console.log(err));
+}).then(() => console.log("Connected to database")).catch((err) => {
+    console.log(err)
+    console.log("MONGO_URI=", process.env.MONGO_URI);
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
